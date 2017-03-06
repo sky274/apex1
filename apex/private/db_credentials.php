3554264ac11f943fe546a1a2c72732acf66a3d3d
Kevin Yoo <yook94@Kevins-MacBook-Pro-2.local>
2017-03-05 21:33:02 -0600

    finished project

--- apex/private/db_credentials.php
+++ apex/private/db_credentials.php
@@ -1,6 +1,6 @@
 <?php
   define("DB_SERVER", "localhost");
   define("DB_USER", "sky274");
-  define("DB_PASS", "1588Sky.");
+  define("DB_PASS", "personal password");
   define("DB_NAME", "apex");
 ?>
